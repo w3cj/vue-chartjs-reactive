20b4fdb2973ebdbd87bcebeee38c13c20f62ced1
CJ R <cj@null.computer>
2018-06-24 20:15:37 -0400

    :bookmark: Update version

--- dist/vue-chartjs.js
+++ dist/vue-chartjs.js
@@ -118,61 +118,10 @@ var VueCharts = {
 "use strict";
 /* unused harmony export reactiveData */
 /* unused harmony export reactiveProp */
-function dataHandler(newData, oldData) {
-  if (oldData) {
-    var chart = this.$data._chart;
-    var newDatasetLabels = newData.datasets.map(function (dataset) {
-      return dataset.label;
-    });
-    var oldDatasetLabels = oldData.datasets.map(function (dataset) {
-      return dataset.label;
-    });
-    var oldLabels = JSON.stringify(oldDatasetLabels);
-    var newLabels = JSON.stringify(newDatasetLabels);
-
-    if (newLabels === oldLabels && oldData.datasets.length === newData.datasets.length) {
-      newData.datasets.forEach(function (dataset, i) {
-        var oldDatasetKeys = Object.keys(oldData.datasets[i]);
-        var newDatasetKeys = Object.keys(dataset);
-        var deletionKeys = oldDatasetKeys.filter(function (key) {
-          return key !== '_meta' && newDatasetKeys.indexOf(key) === -1;
-        });
-        deletionKeys.forEach(function (deletionKey) {
-          delete chart.data.datasets[i][deletionKey];
-        });
-
-        for (var attribute in dataset) {
-          if (dataset.hasOwnProperty(attribute)) {
-            chart.data.datasets[i][attribute] = dataset[attribute];
-          }
-        }
-      });
-
-      if (newData.hasOwnProperty('labels')) {
-        chart.data.labels = newData.labels;
-      }
-
-      if (newData.hasOwnProperty('xLabels')) {
-        chart.data.xLabels = newData.xLabels;
-      }
-
-      if (newData.hasOwnProperty('yLabels')) {
-        chart.data.yLabels = newData.yLabels;
-      }
-
-      chart.update();
-    } else {
-      if (chart) {
-        chart.destroy();
-      }
-
-      this.renderChart(this.chartData, this.options);
-    }
+function dataHandler() {
+  if (this.$data._chart) {
+    this.$data._chart.update();
   } else {
-    if (this.$data._chart) {
-      this.$data._chart.destroy();
-    }
-
     this.renderChart(this.chartData, this.options);
   }
 }
